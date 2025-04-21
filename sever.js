7a5828f039e5b5ce4bc48e4a60ee6374f4d70b93
Duy Lâm Nguyễn <Duylam3300@gmail.com>
2025-04-21 16:24:03 +0700

    404 handle

--- sever.js
+++ sever.js
@@ -1,10 +1,31 @@
-const express=require("express");
-const app= express();
-const port=3000;
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello word");
 })
-app.listen(port,()=>{
+
+
+
+
+//default
+app.all("{*splash}", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "root", "404.html"))
+    }
+    else if (req.accepts("json")) {
+        res.json({ error: "404 not found." })
+    } else {
+        res.type("txt").send("404 not found.")
+    }
+
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+

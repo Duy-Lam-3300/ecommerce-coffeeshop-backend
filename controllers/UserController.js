49f099547ee6ae90241c9236f1fb5de54684c710
Duy Lâm Nguyễn <Duylam3300@gmail.com>
2025-07-01 19:44:01 +0700

    done with sign up func

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,6 +18,8 @@ const getAll = async (req, res) => {
 
 const create = async (req, res) => {
     const { email, password, displayName } = req.body;
+    console.log(req);
+
     if (!email || !password) {
         return res.status(400).json({ error: "Email and password are required." });
     }
@@ -32,8 +34,8 @@ const create = async (req, res) => {
         });
 
     } catch (err) {
-        console.log("Firebase create user error", error);
-        res.status(500).json({ error: error.message });
+        console.log("Firebase create user error", err);
+        res.status(500).json({ err: err.message });
 
     }
 
